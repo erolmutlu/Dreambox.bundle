6f34613aeca03e9ffa5b2cc1c6bff71c89b18a4f
greeeny101 <marc.greenwood574@gmail.com>
2013-12-10 22:59:28 +0000

    Updated to work on iOS.
    Now tested working on Samsung 2012 Smart TV, iOS Webkit, Roku
    
    Tested on Android but not working as of yet

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,36 +1,155 @@
+
+
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
+LIVE_TV = 'live_tv.png'
+RECORDED = 'recorded.png'
 ZAP_TO_URL = 'http://%s:%s/web/zap?sRef=%s'
 STREAM_URL = 'http://%s:%s/%s'
 EPG_URL = 'http://%s:%s/web/epgnow?bRef=%s'
 
+
+
+
+
+
 ####################################################################################################
 def Start():
 
-	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-	ObjectContainer.art = R(ART)
-	ObjectContainer.title1 = 'Dreambox'
-	DirectoryObject.thumb = R(ICON)
+    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items', cols=1)
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = 'Dreambox'
+    DirectoryObject.thumb = R(ICON)
+
+
 
 ####################################################################################################
 @handler('/video/dreambox', 'Dreambox', art=ART, thumb=ICON)
 def MainMenu():
+    #TODO Need to get breadscrumbs sorted out
 
-        oc = ObjectContainer(view_group='List', no_cache=True)
+    oc = ObjectContainer(view_group='InfoList', no_cache=True)
 
-        if Prefs['host'] and Prefs['port_web'] and Prefs['port_video']:
-                url = 'http://{}:{}/web/getservices'.format(Prefs['host'], Prefs['port_web'] )
-                try:
-                        urlHtml = HTML.ElementFromURL(url)
-                except:
-                        Log("Couldn't connect to Dreambox.") 
-                        return None     
-                ServiceReference = urlHtml.xpath("//e2servicereference/text()")
-                ServiceName = urlHtml.xpath("//e2servicename/text()")
-                for item in xrange(len(ServiceReference)):
-					oc.add(DirectoryObject(key = Callback(BouquetsMenu, sender = ServiceName[item], index=ServiceReference[item], name=ServiceName[item]),title = ServiceName[item]))
-        oc.add(PrefsObject(title='Preferences', thumb=R('icon-prefs.png')))      
-        return oc
+    oc.add(DirectoryObject(key=Callback(Bouquets),
+                               title='Live TV',
+                               tagline='Watch live TV direct from your Enigma 2 based satellite receiver',
+                               thumb=R(LIVE_TV),
+                               art=R(LIVE_TV),
+                               duration=100)
+    )
+    oc.add(DirectoryObject(key='http://www.google.co.uk',
+                               title='Recorded TV',
+                               tagline='Watch recorded content on your Enigma 2 based satellite receiver',
+                               thumb=R(RECORDED),
+                               art=R(RECORDED),
+                               duration=100)
+    )
+    oc.add(PrefsObject(title='Preferences', thumb=R('icon-prefs.png')))
+    return oc
+
+
+@route("/video/dreambox/Bouquets")
+def Bouquets():
+    from enigma2 import get_bouquets
+    Log('Entering Bouquets to fetch bouquets from sat box')
+
+    oc = ObjectContainer(view_group='InfoList', title1='Bouquets')
+    bouquets = None
+    if Prefs['host'] and Prefs['port_web']:
+        bouquets = get_bouquets(Prefs['host'], Prefs['port_web'])
+    if bouquets and bouquets[0][0] != 'Error':
+        for name, ref in bouquets:
+            oc.add(DirectoryObject(key=Callback(Channels, bref=ref),
+                               title=name)
+            )
+    else:
+        Log.Error(bouquets)
+    return oc
+
+# need to get the channels and store them as movie objects.
+#can store the picons in them
+# THe urls will point to the UrlService, which will get the epg data for the episodes in the channel
+@route("/video/dreambox/Channels/{bref}")
+def Channels(bref):
+    from enigma2 import get_channels
+    Log('Entering Channels to fetch channels from sat box')
+
+    oc = ObjectContainer(view_group='InfoList', title1='Channels')
+    channels = None
+    if Prefs['host'] and Prefs['port_web']:
+        channels = get_channels(Prefs['host'], Prefs['port_web'], bref)
+    if channels and channels[0] != 'Error':
+        Log(channels[0])
+        for id, start, duration, current_time, title, description, sRef, name in channels:
+            remaining = ((current_time + duration) - current_time) * 1000
+            oc.add(DirectoryObject(key=Callback(ChannelContent, sRef=sRef),
+                                   title='{}  - {}'.format(name, title),
+                                   duration=remaining
+                                   )
+                  )
+    else:
+        Log.Error(channels)
+    return oc
+
+
+
+@route("/video/dreambox/ChannelContent/{sRef}")
+def ChannelContent(sRef):
+    from enigma2 import get_nownext
+    import time
+    Log('Entering ChannelContent to fetch channel contents from sat box')
+
+    oc = ObjectContainer(view_group='InfoList', title1='Channel Content')
+    channel_content = None
+    if Prefs['host'] and Prefs['port_web']:
+        #This is rhe same as ITV player parsing the episodes
+        channel_content = get_nownext(Prefs['host'], Prefs['port_web'], sRef)
+        Log(channel_content)
+    if channel_content and channel_content[0] != 'Error':
+        for id, start, duration, current_time, title, description, sRef, name in channel_content:
+            remaining = ((current_time + duration) - current_time) * 1000
+            # TODO We need to create video clip objects here that pass the url of our service in
+            # TODO I ONLY NEED TO SET THE VURRENT VIDEO TO THR URL AS THE OTHERD DONT EXIST. REREOUTE THEM TO ANOTHER CALLBACK AND HANDLE THE TIMER
+            # TODO Could we use the epg search to search for name etc to retreive it again when we press play
+            # TODO here we will see the full description etc. .....Or event type now or future ??
+            # TODO We need to figure out JUST PASS AN EXTRA PARAM INTO THE THING HTTP192.....?NOW=TRUE, NEXT=FALSE ETC.
+            # todo wE CAN THEN FIGURE OUT WHAT CALL TO DO IN THE URL TO GET DATA AGAIN
+            if int(start) < time.time():
+                oc.add(EpisodeObject(url='http://192.168.1.252/web/epgservicenow?sRef=1:0:1:1933:7FF:2:11A0000:0:0:0:',
+                                   title='{}  - {}'.format(name, title),
+                                   summary=description
+                                   )
+                  )
+            else:
+                # Go off and set a timer
+                oc.add(DirectoryObject(key=Callback(TimerPopup, name=name), title=title,
+                summary="Click here to search for stuff")
+
+    )
+    else:
+        Log.Error(channel_content)
+    return oc
+
+
+@route("/video/dreambox/TimerPopup")
+def TimerPopup(name):
+
+
+
+    return MessageContainer(
+          "Timer",
+          "Timer created for {}".format(name)
+      )
+
+
+
+
+
+
+
+
+
+
 
 
 @route("/video/dreambox/BouquetsMenu/{name}")
@@ -117,7 +236,7 @@ def BouquetsMenu(sender, index, name):
 ##11111##################################################################################################
 @route("/video/dreambox/TvStationMenu")
 def TvStationMenu(sender, channel, epgdescription, epgduration, thumb=R(ICON), include_oc=False):
-    browsers = ('Chrome', 'Internet Explorer', 'Opera', 'Safari', 'iOS')
+    browsers = ('Chrome', 'Internet Explorer', 'Opera', 'Safari', 'iOS', 'Plex Home Theater')
     video_codec = 'h264'
     audio_codec = 'mp3'
     container = 'mp4'
@@ -130,6 +249,7 @@ def TvStationMenu(sender, channel, epgdescription, epgduration, thumb=R(ICON), i
 			thumb=R(piconfile)
     # Set default container for MP4 to work on Samsung.. and others???
 	# Just filters against browser name (Better way to do this ? Check the caps of the connected device?)
+    Log('******' + str(Client.Platform))
     if (Client.Platform  in browsers ):
         container = 'mpegts'
     video = VideoClipObject(
---
+++ Contents/Code/enigma2.py
@@ -0,0 +1,107 @@
+
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+
+def get_bouquets(host, web):
+    bouquets = []
+    url = 'http://{}:{}/web/getservices'.format(host, web)
+    try:
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        soup = BeautifulSoup(response.read())
+        for elems in soup.findAll('e2service'):
+            bref, name = elems.findAll(['e2servicereference', 'e2servicename'])
+            bouquets.append((name.string, bref.string))
+    except Exception as e:
+        return 'Error', 'Messsage and vals : {} host: {} port: {}'.format(e.message, host, web)
+    return bouquets
+
+
+
+def get_channels(host, web, bRef):
+    import urllib
+    channels = []
+    url = 'http://{}:{}/web/epgbouquet'.format(host, web)
+    data = urllib.urlencode({'bRef': bRef})
+    try:
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        soup = BeautifulSoup(response.read())
+        for elems in soup.findAll('e2event'):
+            id, start, duration, current_time, title, description, sRef, name = elems.findAll(['e2eventid',
+                                                                            'e2eventstart',
+                                                                            'e2eventduration',
+                                                                            'e2eventcurrenttime',
+                                                                            'e2eventtitle',
+                                                                            'e2eventdescription',
+                                                                            'e2eventservicereference',
+                                                                            'e2eventservicename'])
+
+            channels.append((int(format_string(id)),
+                             int(format_string(start)),
+                             int(format_string(duration)),
+                             int(format_string(current_time)),
+                             format_string(title),
+                             format_string(description),
+                             format_string(sRef),
+                             format_string(name)))
+
+    except Exception as e:
+        return 'Error', 'Messsage and vals : {} host: {} port: {} bref: {} url: {}'.format(e.message, host, web, bRef, url)
+    return channels
+
+def get_nownext(host, web, sRef):
+    import urllib
+    channels = []
+    url = 'http://{}:{}/web/epgservicenow'.format(host, web)
+    url2 = 'http://{}:{}/web/epgservicenext'.format(host, web)
+    data = urllib.urlencode({'sRef': sRef})
+
+    try:
+        soup = get_data((url, data), (url2, data))
+        for elem in soup:
+            id, start, duration, current_time, title, description, sRef, name = elem.findAll(['e2eventid',
+                                                                            'e2eventstart',
+                                                                            'e2eventduration',
+                                                                            'e2eventcurrenttime',
+                                                                            'e2eventtitle',
+                                                                            'e2eventdescription',
+                                                                            'e2eventservicereference',
+                                                                            'e2eventservicename'])
+
+            channels.append((int(format_string(id)),
+                             int(format_string(start)),
+                             int(format_string(duration)),
+                             int(format_string(current_time)),
+                             format_string(title),
+                             format_string(description),
+                             format_string(sRef),
+                             format_string(name)))
+
+    except Exception as e:
+        return 'Error', 'Messsage and vals : {} host: {} port: {} bref: {} url: {}'.format(e.message, host, web, sRef, url)
+    return channels
+
+def format_string(data):
+    if len(data.string) > 0:
+        return data.string
+    return ''
+
+
+
+def get_data(*args):
+    import urllib2
+    results = []
+    for item in args:
+        u = item[0]
+        #TODO Need to do a check here to make sure it exists
+        data = item[1]
+        try:
+            req = urllib2.Request(u, data)
+            response = urllib2.urlopen(req)
+            soup = BeautifulSoup(response.read())
+            results.append(soup)
+        except Exception as e:
+            results.append(('Error', e.message))
+    return results
